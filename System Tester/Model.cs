bacd6081207ba811d36d1b2f61e3a0c35c384600
Stanislav <Infro-Storm@yandex.ru>
2017-05-21 20:37:28 +0300

    -Реализовано получение данных об ОЗУ

--- System Tester/Model.cs
+++ System Tester/Model.cs
@@ -32,6 +32,64 @@ namespace System_Tester
         storage,
         network
     }
+
+    public enum MemoryFormFactor : byte
+    {
+        Unknown = 0,
+        Other = 1,
+        SIP = 2,
+        DIP = 3,
+        ZIP = 4,
+        SOJ = 5,
+        Proprietary = 6,
+        SIMM = 7,
+        DIMM = 8,
+        TSOP = 9,
+        PGA = 10,
+        RIMM = 11,
+        SODIMM = 12,
+        SRIMM = 13,
+        SMD = 4,
+        SSMP = 15,
+        QFP = 16,
+        TQFP = 17,
+        SOIC = 18,
+        LCC = 19,
+        PLCC = 20,
+        BGA = 21,
+        FPBGA = 22,
+        LGA = 23
+    }
+    public enum MemoryType : byte
+    {
+        Unknown = 0,
+        Other = 1,
+        DRAM = 2,
+        SynchronousDRAM = 3,
+        CacheDRAM = 4,
+        EDO = 5,
+        EDRAM = 6,
+        VRAM = 7,
+        SRAM = 8,
+        RAM = 9,
+        ROM = 10,
+        Flash = 11,
+        EEPROM = 12,
+        FEPROM = 13,
+        EPROM = 14,
+        CDRAM = 15,
+        RAM3D = 16,
+        SDRAM = 17,
+        SGRAM = 18,
+        RDRAM = 19,
+        DDR = 20,
+        DDR2 = 21,
+        DDR2FB = 22,
+        DIMM = 23,
+        DDR3 = 24,
+        FBD2 = 25,
+    }
+
     static class Model
     {
         //Глобальные переменные
@@ -52,6 +110,14 @@ namespace System_Tester
             {
                 return loggerWindow;
             }
+        }
+        public static string ValueСonvert(Int64 value, string units)
+        {
+            if (value > 1099511627776)  return (value / 1099511627776).ToString() + " Т" + units;   //Тера
+            if (value > 1073741824)     return (value / 1073741824).ToString() + " Г" + units;      //Гига
+            if (value > 1048576)        return (value / 1048576).ToString() + " М" + units;         //Мега
+            if (value > 1024)           return (value / 1024).ToString() + " К" + units;            //Кило
+            return value.ToString() + " " + units;
         }
 
         //
@@ -91,6 +157,12 @@ namespace System_Tester
                 mainView.SetInfo(cpu.GetShortInfo(), TabOfProgramm.general);
                 mainView.SetInfo(cpu.GetInfo(), TabOfProgramm.cpu);
             }
+
+            foreach (RAMData ram in GetRAMData())
+            {
+                mainView.SetInfo(ram.GetShortInfo(), TabOfProgramm.general);
+                mainView.SetInfo(ram.GetInfo(), TabOfProgramm.ram);
+            }
         }
         private static List<CPUData> GetCPUData()
         {
@@ -99,17 +171,37 @@ namespace System_Tester
             foreach (ManagementObject instance in searcher.Get()) CPUs.Add(new CPUData(instance));
             return CPUs;
         }
+        private static List<RAMData> GetRAMData()
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", "Select * From Win32_PhysicalMemory");
+            List<RAMData> RAMs = new List<RAMData>();
+            foreach (ManagementObject instance in searcher.Get()) RAMs.Add(new RAMData(instance));
+            return RAMs;
+        }
     }
 
     class Device {
-        protected string deviceName;
-        public Device(ManagementObject instance) {
-            if (instance["DeviceID"] != null)  deviceName = instance["DeviceID"].ToString();
+        public Device(ManagementObject instance)
+        {
         }
         public virtual List<DeviceForView> GetInfo()
         {
             List<DeviceForView> result = new List<DeviceForView>();
-            result.Add(new DeviceForView("Идентификатор устройства", deviceName, "") );
+            return result;
+        }
+    }
+
+    class DeviceWithID:Device
+    {
+        protected string deviceName;
+        public DeviceWithID(ManagementObject instance) : base(instance)
+        {
+            if (instance["DeviceID"] != null) deviceName = instance["DeviceID"].ToString();
+        }
+        public override List<DeviceForView> GetInfo()
+        {
+            List<DeviceForView> result = new List<DeviceForView>();
+            result.Add(new DeviceForView("Идентификатор устройства", deviceName, ""));
             return result;
         }
     }
@@ -147,7 +239,7 @@ namespace System_Tester
         }
     }
 
-    class CPUData : Device
+    class CPUData : DeviceWithID
     {
         string modelNameCPU;
         string processorID;
@@ -217,5 +309,94 @@ namespace System_Tester
             result.Add(new DeviceForView("Максимальная частота", maxClockCPU.ToString(), "МГц"));
             return result;
         }
+    }
+
+    class RAMData : Device
+    {
+        MemoryFormFactor fFactor;
+        MemoryType type;
+        string bankLabel;
+        string deviceLocator;
+        string manufacturer;
+        string partNumber;
+        string serialNumber;
+        Int64 capacityRAM;
+        Int32 speed;
+        Int32 configuredClockSpeed;
+
+        public RAMData(ManagementObject instance) :base(instance)
+        {
+            foreach (PropertyData nameprp in instance.Properties)
+            {
+                string propertyName = nameprp.Name;
+                if (instance[propertyName] != null)
+                {
+                    switch (propertyName)
+                    {
+                        case "BankLabel":
+                            bankLabel = instance[propertyName].ToString();
+                            break;
+                        case "Capacity":
+                            capacityRAM = Convert.ToInt64(instance[propertyName].ToString());
+                            break;
+                        case "DeviceID":
+                            break;
+                        case "ConfiguredClockSpeed":
+                            configuredClockSpeed = Convert.ToInt32(instance[propertyName].ToString());
+                            break;
+                        case "Speed":
+                            speed = Convert.ToInt32(instance[propertyName].ToString());
+                            break;
+                        case "FormFactor":
+                            fFactor =(MemoryFormFactor) Convert.ToInt32(instance[propertyName].ToString());
+                            break;
+                        case "MemoryType":
+                            type = (MemoryType) Convert.ToInt32(instance[propertyName].ToString());
+                            break;
+                        case "PartNumber":
+                            partNumber = instance[propertyName].ToString();
+                            break;
+                        case "Manufacturer":
+                            manufacturer = instance[propertyName].ToString();
+                            break;
+                        case "SerialNumber":
+                            serialNumber = instance[propertyName].ToString();
+                            break;
+                        case "DeviceLocator":
+                            deviceLocator = instance[propertyName].ToString();
+                            break;
+                        default:
+                            Logger.AddText(nameprp.Name + " " + instance[nameprp.Name].ToString(), Message_level.debug, Message_type.info);
+                            break;
+                    }
+
+                }
+            }
+        }
+
+        public override List<DeviceForView> GetInfo()
+        {
+            List<DeviceForView> result = base.GetInfo();
+            result.Add(new DeviceForView("Производитель", manufacturer, ""));
+            result.Add(new DeviceForView("Модель", partNumber, ""));
+            result.Add(new DeviceForView("Установленная частота", configuredClockSpeed.ToString(), "МГц"));
+            result.Add(new DeviceForView("Частота", speed.ToString(), "МГц"));
+            result.Add(new DeviceForView("Объем памяти модуля", Model.ValueСonvert( capacityRAM, "б") , ""));//Величина генерируется конвертером
+            result.Add(new DeviceForView("Банк памяти", bankLabel, ""));
+            result.Add(new DeviceForView("Слот", deviceLocator, ""));
+            result.Add(new DeviceForView("Форм-фактор", fFactor.ToString(), ""));
+            result.Add(new DeviceForView("Тип памяти", type.ToString(), ""));
+            result.Add(new DeviceForView("Серийный номер", serialNumber, ""));
+            return result;
+        }
+        public List<DeviceForView> GetShortInfo()
+        {
+            List<DeviceForView> result = base.GetInfo();
+            result.Add(new DeviceForView("Производитель", manufacturer, ""));
+            result.Add(new DeviceForView("Установленная частота", configuredClockSpeed.ToString(), "МГц"));
+            result.Add(new DeviceForView("Объем памяти модуля", Model.ValueСonvert(capacityRAM, "б"), ""));//Величина генерируется конвертером
+            return result;
+        }
+        
     }
 }
